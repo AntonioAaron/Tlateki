32ef34ae4645c56af42ec2b09f84785687eabad8
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-33.local>
2024-10-29 10:20:47 -0600

    Inicio App

---
+++ AppTlateki/AppTlateki/AppTlatekiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTlatekiApp.swift
+//  AppTlateki
+//
+//  Created by Facultad de Contaduría y Administración on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTlatekiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTlateki/AppTlateki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTlateki
+//
+//  Created by Facultad de Contaduría y Administración on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
